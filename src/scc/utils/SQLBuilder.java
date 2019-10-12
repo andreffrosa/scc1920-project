5f78c1dd576fc5ab03cbbdd08f7bc5ee50675045
Ruben Silva <rfc.silva@campus.fct.unl.pt>
2019-10-12 18:36:09 +0100

    Added config File

--- src/scc/utils/SQLBuilder.java
+++ src/scc/utils/SQLBuilder.java
@@ -2,8 +2,20 @@ package scc.utils;
 
 public class SQLBuilder {
 
+    private static final String SELECT = "SELECT ";
+
     public static String select(String[] fields){
-        return null;
+        String toReturn = SELECT;
+
+        for (String field: fields) {
+            toReturn = toReturn + " " + field +", ";
+        }
+
+        return toReturn;
+    }
+
+    public static String select(String field){
+        return String.format("%s %s", SELECT, field);
     }
 
 }
