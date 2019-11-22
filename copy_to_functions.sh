85b3a82d52ae76ce9c8d16a54af9ae8628407b7a
André Rosa <af.rosa@campus.fct.unl.pt>
2019-11-22 18:28:38 +0000

    saving

--- copy_to_functions.sh
+++ copy_to_functions.sh
@@ -1,5 +1,6 @@
-mkdir -p ../scc1920-project-serverless/src/main/java/resources && cp -r ./src/main/resources/* ../scc1920-project-serverless/src/main/java/resources
-mkdir -p ../scc1920-project-serverless/src/main/java/scc/resources && cp -r ./src/scc/resources/* ../scc1920-project-serverless/src/main/java/scc/resources
-mkdir -p ../scc1920-project-serverless/src/main/java/scc/models && cp -r ./src/scc/models/* ../scc1920-project-serverless/src/main/java/scc/models
-mkdir -p ../scc1920-project-serverless/src/main/java/scc/storage && cp -r ./src/scc/storage/* ../scc1920-project-serverless/src/main/java/scc/storage
-mkdir -p ../scc1920-project-serverless/src/main/java/scc/utils && cp -r ./src/scc/utils/* ../scc1920-project-serverless/src/main/java/scc/utils
+SERVERLESS_DIR=../scc1920-project-serverless
+mkdir -p $SERVERLESS_DIR/src/main/resources && cp -r ./src/main/resources/* $SERVERLESS_DIR/src/main/resources
+mkdir -p $SERVERLESS_DIR/src/scc/resources && cp -r ./src/scc/resources/* $SERVERLESS_DIR/src/scc/resources
+mkdir -p $SERVERLESS_DIR/src/scc/models && cp -r ./src/scc/models/* $SERVERLESS_DIR/src/scc/models
+mkdir -p $SERVERLESS_DIR/src/scc/storage && cp -r ./src/scc/storage/* $SERVERLESS_DIR/src/scc/storage
+mkdir -p $SERVERLESS_DIR/src/scc/utils && cp -r ./src/scc/utils/* $SERVERLESS_DIR/src/scc/utils
